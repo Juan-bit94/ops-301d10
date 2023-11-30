235d10c5be2a46642910e46ff0d1237b27957e26
Juan <cruz59349@gmail.com>
2023-11-30 14:54:41 -0800

    This code upload is for an ops challenge 04 .

---
+++ Menu-system.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+# Script Name:                  Menu-system.sh
+# Author:                       Juan Maldonado
+# Date of lastest revision:     11/30/2023
+# Purpose:                      This script uses conditionals to allow individuals to ping.
+
+# Main
+
+# This will display menu options and accpet user choice
+while true;do 
+
+# Menu options
+echo -e "\nMenu Options:"
+echo "1. Print Hello World"
+echo "2. Pring self (loopback address)"
+echo "3. IP info (network adpater information for this host)"
+echo "4. Exit program"
+
+# User makes a choice 
+read -p "Please enter you choice (1-4): " choice
+
+# Executes user choice
+case $choice in 
+    1)
+        echo "Hello World!"
+        echo " " 
+        echo "You will now be taken back to the menu."
+        echo "Please select another option."
+        ;;
+    2)
+        ping -c  6 127.0.01
+        echo " " 
+        echo "You will now be taken back to the menu."
+        echo "Please select another option."
+        ;;
+    3) 
+        ifconfig
+        echo " " 
+        echo "You will now be taken back to the menu."
+        echo "Please select another option."
+        ;;
+    4)
+        echo " " 
+        echo "You will now exit the program."
+        exit 0
+        ;;
+    *)
+        echo " " 
+        echo "You have entered an invalid choice. Please enter a number between 1 and 4"
+        ;;
+    esac
+done 
