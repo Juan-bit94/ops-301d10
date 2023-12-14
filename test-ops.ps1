d86ee51d17e9be31ff06571f3a1101005764f7e2
Juan Maldonado <77375345+Juan-bit94@users.noreply.github.com>
2023-12-13 18:33:39 -0800

    Create test-ops.ps1

---
+++ test-ops.ps1
@@ -0,0 +1,19 @@
+# Define user information
+$firstName = "Franz"
+$lastName = "Ferdinand"
+$displayName = "$firstName $lastName"
+$username = "ferdi"
+$email = "ferdi@GlobeXpower.com"
+$office = "Springfield"
+$department = "TPS"
+$jobTitle = "TPS Reporting Lead"
+$company = "GlobeX USA"
+
+# Specify the organizational unit (OU) where the user will be created
+$ou = "OU=Test-Generated-Users,DC=CORP,DC=GLOBEXPOWER,DC=COM"
+
+# Generate a password for the new user
+$password = ConvertTo-SecureString -AsPlainText "P@ssw0rd" -Force
+
+# Create a new user in Active Directory
+New-ADUser -SamAccountName $username -UserPrincipalName "$username@$env:USERDNSDOMAIN" -Name $displayName -GivenName $firstName -Surname $lastName -EmailAddress $email -Office $office -Department $department -Title $jobTitle -Company $company -Enabled $true -PasswordNeverExpires $true -ChangePasswordAtLogon $false -AccountPassword $password -Path $ou
